9ec0f75af55acd4acf4a771c8d6a3e44d26c4ec7
MiguelandToolio <60894841+MiguelandToolio@users.noreply.github.com>
2020-02-27 13:32:21 -0500

    Added me code
    I have no idea about some of things you included in here, so yeah it don't work. :)

--- hangingmanofbabylon.py
+++ hangingmanofbabylon.py
@@ -1,21 +1,44 @@
 from __future__ import print_function
-updatedSpaces=['-','-','-','-','-']
-word='steve'
-lives=5
+word = 'earring'
+updatedSpaces = [('_ ') * len(word)]
+
+def initialize():
+    print (''.join(updatedSpaces))
+    print('Lives = 6')
+    getLetter()
+def getLetter():
+    global letter
+    letter = raw_input('Your guess: ')
+    check(letter)
+    getLetter()
+    
 def check(letter):
     global lives
     global updatedSpaces
     global word
     if letter in word:
         for i in range(0,len(word)-1):
-            Hamza=word.find(letter, i, len(word))
-            if Hamza != -1:
-                updatedSpaces[Hamza]=letter
+            earring=word.find(letter, i, len(word))
+            if earring != -1:
+                updatedSpaces[earring]=letter
     else:
         lives=lives-1
         print('Not in word,', lives, 'lives left!')
     print(updatedSpaces)
+    getLetter()
+def won ():
+    if ('_') in updatedSpaces:
+        getletter()
+    else:
+        print('You have guessed the word! Congradulations, you have won!')
         
+def main():
+    initialize()
+
+main()
+
+
+
         
         
         
